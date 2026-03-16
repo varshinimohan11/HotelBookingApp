6f450fd6da2ed2db53a74dbbec4e8c47576deb95
Varshini Mohan <vm2817@srmist.edu.in>
2026-03-16 20:33:18 +0530

    Added UseCase1 Welcome Message

---
+++ App/src/UseCase1WelcomeMessage.java
@@ -0,0 +1,12 @@
+public class UseCase1WelcomeMessage {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     Welcome to Book My Stay     ");
+        System.out.println("     Hotel Booking System v1.0   ");
+        System.out.println("=================================");
+        System.out.println("Application Started Successfully");
+
+    }
+}
