7707ff343a888d0d9f9ef11f807f508a52de3aa3
Varshini Mohan <vm2817@srmist.edu.in>
2026-03-31 00:07:57 +0530

    Added Use Case 6 - Reservation Confirmation and Room Allocation

--- App/src/usecase6RoomAllocation.java
+++ App/src/usecase6RoomAllocation.java
@@ -1,2 +1,32 @@
-public class usecase6RoomAllocation {
+public class RoomSearchService {
+
+    private RoomInventory inventory;
+
+    public RoomSearchService(RoomInventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public void searchAvailableRooms() {
+
+        System.out.println("\nAvailable Rooms:\n");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        checkAndDisplay(single);
+        checkAndDisplay(doubleRoom);
+        checkAndDisplay(suite);
+    }
+
+    private void checkAndDisplay(Room room) {
+
+        int available = inventory.getAvailability(room.getRoomType());
+
+        if (available > 0) {
+            room.displayRoomDetails();
+            System.out.println("Available Rooms: " + available);
+            System.out.println();
+        }
+    }
 }
